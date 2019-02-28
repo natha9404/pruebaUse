0986f537ad19a5e99b2853594a7917e5d5d4c0c4
natha9404 <natha9404@gmail.com>
2019-02-28 17:40:50 -0500

    modelos usuarios, tablero y tarjetas

--- tableros/models.py
+++ tableros/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import Usuario
 
 # Create your models here.
+
+ESTADO_CHOICES = (('Privado', 'Privado'), ('Público', 'Público'))
+
+class Tablero(models.Model):
+    nombreTablero = models.CharField(max_length=100, unique=True)
+    estado = models.CharField(max_length=20, verbose_name="estado", default='Privado', choices=ESTADO_CHOICES)
+    propietario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombreTablero
+
+
+class Card(models.Model):
+    titulo = models.CharField(max_length=100, unique=True)
+    contenido = models.TextField()
+    ultimaModificacion = models.DateTimeField(auto_now_add=False, default=False, editable=False)
+    tablero = models.ForeignKey(Tablero, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
