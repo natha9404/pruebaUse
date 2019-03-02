93c9e776b2538efea5471c10eb7fee6db74e9e0c
natha9404 <natha9404@gmail.com>
2019-03-02 10:36:18 -0500

    cambio modelo usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -8,11 +8,19 @@ alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Unicamente se aceptan caracter
 alpha = RegexValidator(r'^[a-zA-Z]*$', 'Unicamente se aceptan caracteres alfanumericos.')
 numeric = RegexValidator(r'^[0-9]*$', 'Solamente valores númericos')
 
+
+def get_upload_to(instance, filename):
+    return 'upload/%s/%s' % (instance.numero_documento_identificacion, filename)
+
 # Create your models here.
 class Usuario (User):
     Token_validity_duration = timedelta(days=7)
     segundo_nombre = models.CharField(max_length=100, verbose_name="Segundo Nombre", blank=True, validators=[alpha])
     segundo_apellido = models.CharField(max_length=100, verbose_name="Segundo Apellido", blank=True, validators=[alpha])
+    numero_documento_identificacion = models.CharField(max_length=20,
+                                                       verbose_name="Número de documento de identificación",
+                                                       validators=[numeric])
+    foto = models.ImageField(upload_to=get_upload_to, null=True, blank=True)
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
